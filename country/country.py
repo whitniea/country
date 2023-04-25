ca8f4640bae08c59af9ef1543286b9aa7cad17db
username <useremail>
2023-04-25 11:52:01 -0700

    kjjlh

--- country/country.py
+++ country/country.py
@@ -1,1 +1,15 @@
 #whitnie allison cis 261 country
+print("welcome to a country list program\n")
+
+country = ['Italy', 'Switzerland', 'Russia', 'Germany'] 
+input("COMMAND MENU")
+if input == list:
+    print(country)
+
+   
+
+
+
+
+
+
