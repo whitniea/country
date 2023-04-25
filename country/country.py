092bfafaa50eb3d953edb96b40a531f0911c0fbf
username <useremail>
2023-04-25 12:11:20 -0700

    hgtfv

--- country/country.py
+++ country/country.py
@@ -1,10 +1,18 @@
 #whitnie allison cis 261 country
 print("welcome to a country list program\n")
+print("what countries would you like to visit in your lifetime?")
 
-country = ['Italy', 'Switzerland', 'Russia', 'Germany'] 
-input("COMMAND MENU")
+
+print("\nCOMMAND MENU")
+print("list - list all countries")
+print("add - add a country")
+print("del - delete a country")
+print("exit - exit program\n")
+input("what would you like to do?:  ")
+
+list = ["France", "Italy", "Japan", "England"]
 if input == list:
-    print(country)
+    print(list)
 
    
 
