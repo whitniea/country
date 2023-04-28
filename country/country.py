2ada8736dc514608038633e1441292d689aa1762
waall <waall@LAPTOP>
2023-04-27 19:13:01 -0500

    hgvgkv

--- country/country.py
+++ country/country.py
@@ -1,22 +1,76 @@
 #whitnie allison cis 261 country
-print("welcome to a country list program\n")
-print("what countries would you like to visit in your lifetime?")
+
+def display_menu():
+    print("COMMAND MENU")
+    print("view - View country name")
+    print("add  - Add a country")
+    print("del  - Delete a country")
+    print("exit - Exit program")
+    print()
+
+def display_codes(countries):
+    codes = list(countries.keys())
+    codes.sort()
+    codes_line = "Country codes: "
+    for code in codes:
+        codes_line += code + " "
+    print(codes_line)
+
+def view(countries):
+    display_codes(countries)
+    code = input("Enter country code: ")
+    code = code.upper()
+    if code in countries:
+        name = countries[code]
+        print(f"Country name: {name}.\n")
+    else:
+        print("There is no country with that code.\n")
+
+def add(countries):
+    code = input("Enter country code: ")
+    code = code.upper()
+    if code in countries:
+        name = countries[code]
+        print(f"{name} is already using this code.\n")
+    else:
+        name = input("Enter country name: ")
+        name = name.title()
+        countries[code] = name
+        print(f"{name} was added.\n")
+
+def delete(countries):
+    code = input("Enter country code: ")
+    code = code.upper()
+    if code in countries:
+        name = countries.pop(code)
+        print(f"{name} was deleted.\n")
+    else:
+        print("There is no country with that code.\n")
+
+def main():
+    countries = {"CA": "Canada",
+                 "US": "United States",
+                 "MX": "Mexico"}
+    
+    display_menu()
+    while True:        
+        command = input("Command: ")
+        command = command.lower()
+        if command == "view":
+            view(countries)   
+        elif command == "add":
+            add(countries)
+        elif command == "del":
+            delete(countries)  
+        elif command == "exit":
+            print("Bye!")
+            break
+        else:
+            print("Not a valid command. Please try again.\n")
 
 
-print("\nCOMMAND MENU")
-print("list - list all countries")
-print("add - add a country")
-print("del - delete a country")
-print("exit - exit program\n")
-input("what would you like to do?  ")
-
-country = ["Japan", "England", "France", "Italy"]
-
-if input == lista:
-    print(country)
-
-   
-
+if __name__ == "__main__":
+    main()
 
 
 
